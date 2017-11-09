59ecba23f79bdbfa2de5d6f9d6c900e61dfe1ae3
thor <Brian Bierlein>
2017-11-08 21:40:48 -0800

    removed unnecessary commented lines

--- vagrinit.py
+++ vagrinit.py
@@ -33,25 +33,3 @@ if __name__ == "__main__":
         print("not enough arguments")
         sys.exit()
     WrVagrInit(sys.argv[1], sys.argv[2])
-
-
-
-
-#Vagrant.configure("2") do |config|
-
-#  config.vm.define "web" do |web|
-#        web.vm.box = "ubuntu/trusty64"
-#        web.vm.network :private_network, ip: "10.0.0.10"
-#        web.vm.hostname = "web"
-#  end
-#  config.vm.define "db" do |db|
-#        db.vm.box = "ubuntu/trusty64"
-#        db.vm.network :private_network, ip: "10.0.0.11"
-#        db.vm.hostname = "db"
-#  end
-#  config.vm.define "gtw1" do |gtw1|
-#        gtw1.vm.box = "ubuntu/trusty64"
-#        gtw1.vm.network :private_network, ip: "10.0.0.12"
-#        gtw1.vm.hostname = "gtw1"
-#  end
-#end
